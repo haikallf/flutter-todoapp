2e328cc27bd314206da060bc126ee85704e61408
haikallf <haikallzrd@gmail.com>
2022-05-26 16:24:50 +0700

    minor fix

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -98,7 +98,7 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
           ),
-          MyButton(label: "+ Add Task", onTap: () => Get.to(AddTaskPage()))
+          MyButton(label: "+ Add Task", onTap: () => Get.to(() => AddTaskPage()))
         ],
       ),
     );
