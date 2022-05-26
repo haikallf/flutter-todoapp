46ed321a7207174127f513d38fa61da0a19c266f
haikallf <haikallzrd@gmail.com>
2022-05-26 17:18:51 +0700

    add remind logic

--- lib/ui/add_task_bar.dart
+++ lib/ui/add_task_bar.dart
@@ -15,6 +15,13 @@ class _AddTaskPageState extends State<AddTaskPage> {
   DateTime _selectedDate = DateTime.now();
   String _startTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
   String _endTime = DateFormat("hh:mm a").format(DateTime.now()).toString();
+  int _selectedRemind = 5;
+  List<int> remindList = [
+    5,
+    10,
+    15,
+    20
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +65,26 @@ class _AddTaskPageState extends State<AddTaskPage> {
                         },
                       )))
                 ],
-              )
+              ),
+              MyInputField(title: "Remind", hint: "$_selectedRemind minutes early", widget: DropdownButton(
+                icon: Icon(Icons.keyboard_arrow_down, color: Colors.grey),
+                iconSize: 32,
+                elevation: 4,
+                style: subTitleStyle,
+                underline: Container(height: 0),
+                items: remindList.map<DropdownMenuItem<String>>((int value) {
+                  return DropdownMenuItem<String>(
+                    value: value.toString(),
+                    child: Text(value.toString()),
+                  );
+                }
+                ).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _selectedRemind = int.parse(newValue!);
+                  });
+                },
+              ),)
             ],
           ),
         ),
