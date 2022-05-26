99689e59855197c35cfce67fc19ecd15fb2251d4
haikallf <haikallzrd@gmail.com>
2022-05-26 16:22:34 +0700

    modified button

--- lib/ui/widgets/button.dart
+++ lib/ui/widgets/button.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:todoapp/ui/theme.dart';
 
 class MyButton extends StatelessWidget {
-  const MyButton({Key? key}) : super(key: key);
+  final String label;
+  final Function()? onTap;
+  const MyButton({Key? key, required this.label, required this.onTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => null,
+      onTap: onTap,
       child: Container(
-        width: 100,
+        alignment: Alignment.center,
+        width: 120,
         height: 60,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(20),
           color: primaryClr
         ),
+        child: Text(label, style: TextStyle(color: Colors.white))
       ),
     );
   }
